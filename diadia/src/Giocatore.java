f78be41070e8b77989da85e201c79271767fec3e
ThatOneStudentTryingToPass <mat.pozza@stud.uniroma3.it>
2026-04-02 21:13:21 +0200

    Update Giocatore.java

--- diadia/src/Giocatore.java
+++ diadia/src/Giocatore.java
@@ -6,5 +6,33 @@
  * @version alpha
  */
 public class Giocatore {
+
+	static final private int CFU_INIZIALI = 20;
 	
+    private int cfu;
+    private borsa borsa;
+
+    public Giocatore() {
+        this.cfu = CFU_INIZIALI;
+        
+        this.borsa = new borsa();
+    }
+
+    public int getCfu() {
+        return cfu;
+    }
+
+    public void setCfu(int cfu) {
+        this.cfu = cfu;
+    }
+
+    public void decrementaCfu() {
+        if(cfu > 0) {
+            cfu--;
+        }
+    }
+
+    public borsa getBorsa() {
+        return borsa;
+    }
 }
